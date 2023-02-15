2232ebab77e70c8902ec138805e31605ba0f7fbc
Sean C. Rife <seanrife@gmail.com>
2023-02-14 20:39:19 -0600

    Iterate over files

--- analyze.py
+++ analyze.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 from ethnicolr import pred_wiki_name
 import os
-
+import gender_guesser.detector as gender
 
 def clean_author_data(authors):
     authors = authors.split('\t')[1]
@@ -20,18 +20,18 @@ def process_data(data):
         authors = json.loads(clean_author_data(datum))
         for author in authors:
             name_list.append({'first': author['given'], 'last': author['family']})
-    return name_list
+
+    return {'doi': datum[0], 'names': name_list, 'sup': datum[2], 'men': datum[3], 'con': datum[4], 'tot': datum[5]}
 
 
-
-directory = os.fsencode("/mnt/c/Users/sean/Documents/author_tallies")
+directory = "/mnt/c/Users/sean/Documents/author_tallies"
 
 for file in os.listdir(directory):
-    with open(file, encoding="utf8") as f:
+    with open("/mnt/c/Users/sean/Documents/author_tallies/" + file, encoding="utf8") as f:
         data = f.readlines()[1:]
     name_list_processed = process_data(data)
 
-    author_df = pd.DataFrame(name_list)
+    author_df = pd.DataFrame(name_list_processed['names'])
     odf = pred_wiki_name(author_df,'last', 'first', conf_int=0.9)
 
 
