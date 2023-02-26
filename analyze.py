0460e82ff6c104ce0b5f6170e82dd8dca64c4583
Sean C. Rife <seanrife@gmail.com>
2023-02-26 17:23:41 -0600

    Parallelize

--- analyze.py
+++ analyze.py
@@ -3,6 +3,14 @@ import pandas as pd
 from ethnicolr import pred_wiki_name
 import os
 import gender_guesser.detector as gender
+import time
+from multiprocessing import Pool
+
+
+def divide_chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
 
 def clean_author_data(authors):
     authors = authors.split('\t')[1]
@@ -15,29 +23,92 @@ def clean_author_data(authors):
 
 
 def process_data(data):
-    name_list = []
+    out_list = []
     for datum in data:
+        #name_list = []
         authors = json.loads(clean_author_data(datum))
+        datum = datum.split('\t')
         for author in authors:
-            name_list.append({'first': author['given'], 'last': author['family']})
+            #name_list.append({'first': author['given'], 'last': author['family']})
+            out_list.append({'doi': datum[0], 'first': author['given'], 'last': author['family'], 'sup': datum[2], 'men': datum[3], 'con': datum[4], 'tot': datum[5][:-1]})
 
-    return {'doi': datum[0], 'names': name_list, 'sup': datum[2], 'men': datum[3], 'con': datum[4], 'tot': datum[5]}
+    return out_list
 
+
+d = gender.Detector()
 
 directory = "/mnt/c/Users/sean/Documents/author_tallies"
 
-for file in os.listdir(directory):
+
+def process_multiple(files):
+    for file in files:
+        process(file)
+
+
+def process(file):
+    with open('output/' + file, 'a') as o:
+        o.write('doi\tauth_number\teth_count\tsex_count\tsup\tmen\tcon\ttot\n')
+
+    start = time.time()
+    print("Processing " + file)
     with open("/mnt/c/Users/sean/Documents/author_tallies/" + file, encoding="utf8") as f:
         data = f.readlines()[1:]
-    name_list_processed = process_data(data)
 
-    author_df = pd.DataFrame(name_list_processed['names'])
-    odf = pred_wiki_name(author_df,'last', 'first', conf_int=0.9)
+    list_processed = process_data(data)
+
+    author_df = pd.DataFrame.from_dict(list_processed)
+
+    eth_df = pred_wiki_name(author_df,'last', 'first', conf_int=0.9)
+
+    unique_dois = eth_df['doi'].unique()
+
+    for unique_doi in unique_dois:
+        rows = eth_df.loc[eth_df['doi'] == unique_doi]
+        eth_sex_summary = []
+        auth_number = 0
+        eth_count = 0
+        sex_count = 0
+        eths = []
+        sexes = []
+        sup = rows['sup'].iloc[0]
+        men = rows['men'].iloc[0]
+        con = rows['con'].iloc[0]
+        tot = rows['tot'].iloc[0]
+        for i, row in rows.iterrows():
+            auth_number = auth_number + 1
+            if row['race'] not in eths:
+                eth_count = eth_count + 1
+                eths.append(row['race'])
+            
+            sex = d.get_gender(row['first']).replace('mostly_', '')
+
+            if sex not in sexes and sex != 'unknown' and sex != 'andy':
+                sex_count = sex_count + 1
+                sexes.append(sex)
+
+        eth_sex_summary.append({'doi': unique_doi[1:-1], 'auth_number': auth_number,
+                                'eth_count': eth_count, 'sex_count': sex_count,
+                                'sup': sup, 'men': men, 'con': con, 'tot': tot})
+    
+        with open('output/' + file, 'a') as o:
+            for record in eth_sex_summary:
+                o.write(record['doi'] + '\t' + str(record['auth_number']) + '\t' +
+                        str(record['eth_count']) + '\t' + str(record['sex_count']) + '\t' +
+                        str(record['sup']) + '\t' + str(record['men']) + '\t' +
+                        str(record['con']) + '\t' + str(record['tot']) + '\n')
 
 
+    end = time.time()
+    print(end-start)
 
-print(odf)
+dir_list = []
 
-end = time.time()
+for file in os.listdir(directory):
+    dir_list.append(file)
 
-print(end-start)
+n = 270
+
+x = list(divide_chunks(dir_list, n))
+
+with Pool(n) as p:
+    p.map(process_multiple, x)
